0255a9454c320680a4ac07012a91f29445c92773
moo25 <ansckdan@gmail.com>
2018-06-27 14:03:35 +0900

    propTypes, defaultProps added

--- src/components/Example/Example.js
+++ src/components/Example/Example.js
@@ -4,12 +4,19 @@ import classNames from 'classnames/bind'
 
 const cx = classNames.bind(styles)
 
-const Example = () => {
+const Example = ({example = f => f}) => {
   return (
     <div>
 
     </div>
   );
 };
+
+Example.propTypes = {}
+
+// Example.propTypes = {
+//   example : f => f
+// }
+
 
 export default Example;
